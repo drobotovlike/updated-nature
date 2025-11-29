51a5ce60008ee15bcd44fea70ae9399ccc98a30c
drobotovlike <42763737+drobotovlike@users.noreply.github.com>
2025-11-29 20:57:33 +0800

    Add explicit file extensions to imports in y-supabase provider

--- src/utils/y-supabase/index.js
+++ src/utils/y-supabase/index.js
@@ -3,11 +3,11 @@
  */
 
 import * as Y from 'yjs'
-import { Awareness, encodeAwarenessUpdate, applyAwarenessUpdate } from 'y-protocols/awareness'
-import { Observable } from 'lib0/observable'
-import * as encoding from 'lib0/encoding'
-import * as decoding from 'lib0/decoding'
-import { writeUpdate, readSyncMessage, writeSyncStep1 } from 'y-protocols/sync'
+import { Awareness, encodeAwarenessUpdate, applyAwarenessUpdate } from 'y-protocols/awareness.js'
+import { Observable } from 'lib0/observable.js'
+import * as encoding from 'lib0/encoding.js'
+import * as decoding from 'lib0/decoding.js'
+import { writeUpdate, readSyncMessage, writeSyncStep1 } from 'y-protocols/sync.js'
 
 const messageSync = 0
 const messageAwareness = 1
